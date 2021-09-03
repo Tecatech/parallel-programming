8d467e674d9be8110e230fed3a680c0f9daa9894
Tecatech <hexdump1337@gmail.com>
2021-09-03 21:50:43 +0300

    resources added

---
+++ warm-up/main.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int nthreads, tid;
+    
+    // начало параллельного региона
+    #pragma omp parallel private(nthreads, tid)
+    {
+        // получение идентификатора потока
+        tid = omp_get_thread_num();
+        printf("Hello from thread = %d\n", tid);
+        
+        if (tid == 0) {
+            // функция главного потока
+            nthreads = omp_get_num_threads();
+            printf("Number of threads = %d\n", nthreads);
+        }
+    }
+    
+    return 0;
+}
